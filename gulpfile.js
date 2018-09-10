7769bd10f4681625789c6cdf34d9b53baa43c392
dmitry-melnishin <dmelnishin@gmail.com>
2018-09-10 10:50:13 +0300

    concat+uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,34 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    concat      = require('gulp-concat'),
+    uglify      = require('gulp-uglify');
+
+gulp.task('sass', function () {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['app/libs/*.js', 'app/js/main.js'])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
 
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+    notify: false
+  });
 });
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-
-    browserSync.init({
-        server: "./app"
-    });
-
-    gulp.watch("app/scss/*.scss", ['sass']);
-    gulp.watch("app/*.html").on('change', browserSync.reload);
-    gulp.watch("app/js/*.js").on('change', browserSync.reload);
+gulp.task('watch', ['browser-sync', 'sass', 'scripts'], function() {
+  gulp.watch('app/scss/*.scss', ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+  gulp.watch("app/js/*.js").on('change', browserSync.reload);
 });
-
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
-        .pipe(sass())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest("app/css"))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('default', ['serve']);
